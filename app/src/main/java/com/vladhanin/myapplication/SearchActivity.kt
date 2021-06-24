06fa7829f7ef4cad86a56720ffc20404a5da6644
firmeldir <drwowwitcher@gmail.com>
2021-06-24 23:43:11 +0300

    add search uses

--- app/src/main/java/com/vladhanin/myapplication/SearchActivity.kt
+++ app/src/main/java/com/vladhanin/myapplication/SearchActivity.kt
@@ -11,32 +11,26 @@ class SearchActivity : AppCompatActivity() {
 
     private lateinit var usersAdapter: UsersAdapter
 
-//    val users = setOf<User>(
-//        User("Дмитро" ,"Ковальчуков"),
-//        User("Олена", "Ковальчук"),
-//        User("Олександр", "Ковальчук")
-//    )
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
 
-//        with(findViewById<RecyclerView>(R.id.usersList)){
-//            adapter = UsersAdapter(context, users.toTypedArray()).also {
-//                usersAdapter = it
-//            }
-//        }
+        with(findViewById<RecyclerView>(R.id.usersList)){
+            adapter = UsersAdapter(context, Data.USERS.toTypedArray()).also {
+                usersAdapter = it
+            }
+        }
 
         findViewById<EditText>(R.id.searchInput).doAfterTextChanged {
             val query = it.toString()
 
-//            val filtered = users.filter { user ->
-//                query.isEmpty() ||
-//                        user.name.startsWith(prefix = query, ignoreCase = true) ||
-//                        user.surname.startsWith(prefix = query, ignoreCase = true)
-//            }
+            val filtered = Data.USERS.filter { user ->
+                query.isEmpty() ||
+                        user.name.startsWith(prefix = query, ignoreCase = true) ||
+                        user.surname.startsWith(prefix = query, ignoreCase = true)
+            }
 
-            usersAdapter.dataSet = emptyArray()
+            usersAdapter.dataSet = filtered.toTypedArray()
             usersAdapter.notifyDataSetChanged()
         }
     }
