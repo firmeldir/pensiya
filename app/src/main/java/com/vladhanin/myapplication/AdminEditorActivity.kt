b9c6bdf0aa9ef2d06bf33f89ae8c6cca5e68b099
Maxim Sutkovenko <mr.gigant977@gmail.com>
2021-06-24 23:27:51 +0300

    цуацуаацу

--- app/src/main/java/com/vladhanin/myapplication/AdminEditorActivity.kt
+++ app/src/main/java/com/vladhanin/myapplication/AdminEditorActivity.kt
@@ -7,6 +7,7 @@ import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.vladhanin.myapplication.models.User
+import java.text.SimpleDateFormat
 
 class AdminEditorActivity : AppCompatActivity() {
 
@@ -27,11 +28,18 @@ class AdminEditorActivity : AppCompatActivity() {
         val edName = findViewById<EditText>(R.id.ed_name)
         val edSurname = findViewById<EditText>(R.id.ed_surname)
         val edPassportId = findViewById<EditText>(R.id.ed_passportId)
+        val edDateOfBirth = findViewById<EditText>(R.id.ed_date_of_birth)
+        val edActualAddress = findViewById<EditText>(R.id.ed_actual_adrress)
+        val edFormalAddress = findViewById<EditText>(R.id.ed_formal_adress)
         val save = findViewById<Button>(R.id.save)
 
         edName.setText(editUser?.name)
         edSurname.setText(editUser?.surname)
-        edPassportId.setText(editUser?.passportId)
+        edPassportId.setText(editUser?.pensionId)
+        val df = SimpleDateFormat("yyyy-mm-dd")
+        edDateOfBirth.setText(df.format(editUser?.dateOfBirthday))
+        edActualAddress.setText(editUser?.actualAddress)
+        edFormalAddress?.setText(editUser?.officialAddress)
 
         val rv = findViewById<RecyclerView>(R.id.ed_jobs)
         rv.adapter = JobEditAdapter(editUser?.jobs ?: mutableListOf())
@@ -39,7 +47,7 @@ class AdminEditorActivity : AppCompatActivity() {
         save.setOnClickListener {
             editUser?.name = edName.text.toString()
             editUser?.surname = edSurname.text.toString()
-            editUser?.passportId = edPassportId.text.toString()
+            editUser?.pensionId = edPassportId.text.toString()
         }
     }
 }
