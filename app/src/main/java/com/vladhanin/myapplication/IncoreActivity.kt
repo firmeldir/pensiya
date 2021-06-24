87d0f0746dd2d9e5de39fa5c4772192b979e1fb2
Maxim Sutkovenko <mr.gigant977@gmail.com>
2021-06-25 00:20:35 +0300

    wefwef

--- app/src/main/java/com/vladhanin/myapplication/IncoreActivity.kt
+++ app/src/main/java/com/vladhanin/myapplication/IncoreActivity.kt
@@ -2,6 +2,7 @@ package com.vladhanin.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 class IncoreActivity : AppCompatActivity() {
@@ -9,8 +10,13 @@ class IncoreActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_incore)
         val rv = findViewById<RecyclerView>(R.id.income)
+        val pv = findViewById<TextView>(R.id.pension_size)
         val adapter = IncomeAdapter()
         rv.adapter = adapter
-        Data.CURRENT_USER?.incomes?.let { adapter.add(it) }
+        Data.CURRENT_USER?.incomes?.let {
+            val sum =  it.map { income -> income.money.toIntOrNull() ?: 0 }.sum()
+            pv.text = (0.85 * sum / it.count()).toInt().toString()
+            adapter.add(it)
+        }
     }
 }
