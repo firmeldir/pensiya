7efa1250a1aeca5c888db751eb867be7a812809f
Maxim Sutkovenko <mr.gigant977@gmail.com>
2021-06-25 00:29:47 +0300

    wefwefwfewf

--- app/src/main/java/com/vladhanin/myapplication/IncoreActivity.kt
+++ app/src/main/java/com/vladhanin/myapplication/IncoreActivity.kt
@@ -1,11 +1,24 @@
 package com.vladhanin.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.vladhanin.myapplication.models.User
 
 class IncoreActivity : AppCompatActivity() {
+
+    companion object {
+
+        var user: User? = null
+
+        fun start(activity: AdminEditorActivity, user: User) {
+            this.user = user
+            activity.startActivity(Intent(activity, IncoreActivity::class.java))
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_incore)
@@ -13,8 +26,10 @@ class IncoreActivity : AppCompatActivity() {
         val pv = findViewById<TextView>(R.id.pension_size)
         val adapter = IncomeAdapter()
         rv.adapter = adapter
-        Data.CURRENT_USER?.incomes?.let {
-            val sum =  it.map { income -> income.money.toIntOrNull() ?: 0 }.sum()
+        val u = if (user == null) Data.CURRENT_USER else user
+
+        u?.incomes?.let {
+            val sum = it.map { income -> income.money.toIntOrNull() ?: 0 }.sum()
             pv.text = (0.85 * sum / it.count()).toInt().toString()
             adapter.add(it)
         }
