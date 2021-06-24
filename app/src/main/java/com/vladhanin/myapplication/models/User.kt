e27db3196dfad394ea976a83e68588f1e4d604a0
firmeldir <drwowwitcher@gmail.com>
2021-06-24 22:51:29 +0300

    point 1.5

--- app/src/main/java/com/vladhanin/myapplication/models/User.kt
+++ app/src/main/java/com/vladhanin/myapplication/models/User.kt
@@ -1,11 +1,21 @@
 package com.vladhanin.myapplication.models
 
+import java.util.*
+
 data class User(
-    val login: String,
-    val name: String,
-    val surname: String,
-    val passportId: String,
-    val incomes: List<Income>,
-    val jobs: List<Job>,
-    val isAdmin: Boolean
+    var login: String,
+    var name: String,
+    var surname: String,
+    var pensionId: String,
+    var dateOfBirthday: Date,
+
+    var officialAddress: String,
+    var actualAddress: String,
+
+    var incomes: List<Income>,
+    var jobs: List<Job>,
+
+    var pension: Pension?,
+
+    var isAdmin: Boolean
 )
